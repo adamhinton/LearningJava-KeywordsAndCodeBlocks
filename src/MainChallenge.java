5e5e23643f4177c03df62f38afa77aa31dfb6549
adamd <adam.d.hinton@gmail.com>
2024-02-11 11:02:09 -0500

    Complete challenge

--- src/MainChallenge.java
+++ src/MainChallenge.java
@@ -1,9 +1,9 @@
 public class MainChallenge {
     public static void main(String[] args) {
         boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+        int score = 10_000;
+        int levelCompleted = 8;
+        int bonus = 200;
 
         int finalScore = score;
 
